6adad70ab632fd2c0a002638427939252ed30126
oasanchez2 <oa.sanchez2@uniandes.edu.co>
2022-11-05 23:04:15 -0500

    Se añaden cálculos del listado de autos

--- src/app/Vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/Vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -12,16 +12,27 @@ export class VehiculoListComponent implements OnInit {
   constructor(private vehiculoService: VehiculoService) { }
 
   vehiculosList: Array<Vehiculo> = [];
+  counterMarcas: any[] = [];
 
   getVehiculos():void{
     this.vehiculoService.getVehiculos().subscribe((data) =>{
       this.vehiculosList = data;
+
+      let marcas = Array.from(new Set(this.vehiculosList.map(x => x.marca)));
+      this.counterMarcas = marcas.map(mar => {
+          return {marca: mar, count: 0};
+      });
+
+     this.counterMarcas.map((countMarc, i) =>{
+          const actualMarcaLength =  this.vehiculosList.filter(number => number.marca === countMarc.marca).length;
+          countMarc.count = actualMarcaLength;
+      })
+
     });
-  }
-
-  calcularEstadistica():void{
 
   }
+
+
 
   ngOnInit() {
     this.getVehiculos();
